d1925b4f46800499fb8a8f952f603c3d0cf7b118
Andyyuda <128485068+Andyyuda@users.noreply.github.com>
2023-07-26 12:01:47 +0700

    Add files via upload

---
+++ addsstp.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+# My Telegram : https://t.me/AnuyBazoelk
+# ==========================================
+# Color
+RED='\033[0;31m'
+NC='\033[0m'
+GREEN='\033[0;32m'
+ORANGE='\033[0;33m'
+BLUE='\033[0;34m'
+PURPLE='\033[0;35m'
+CYAN='\033[0;36m'
+LIGHT='\033[0;37m'
+# ==========================================
+# Getting
+ipsaya=$(wget -qO- ipinfo.io/ip)
+data_server=$(curl -v --insecure --silent https://google.com/ 2>&1 | grep Date | sed -e 's/< Date: //')
+date_list=$(date +"%Y-%m-%d" -d "$data_server")
+data_ip="https://raw.githubusercontent.com/Jengkolonline/izinn/main/ip"
+checking_sc() {
+  useexp=$(wget -qO- $data_ip | grep $ipsaya | awk '{print $3}')
+  if [[ $date_list < $useexp ]]; then
+    echo -ne
+  else
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e "\033[42m          404 NOT FOUND AUTOSCRIPT          \033[0m"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    echo -e ""
+    echo -e "            ${RED}PERMISSION DENIED !${NC}"
+    echo -e "   \033[0;33mYour VPS${NC} $ipsaya \033[0;33mHas been Banned${NC}"
+    echo -e "     \033[0;33mBuy access permissions for scripts${NC}"
+    echo -e "             \033[0;33mContact Admin :${NC}"
+    echo -e "      \033[0;36mTelegram${NC} t.me/Jengkol_Online"
+    echo -e "      ${GREEN}WhatsApp${NC} wa.me/6282372139631"
+    echo -e "\033[1;93m────────────────────────────────────────────\033[0m"
+    exit
+  fi
+}
+checking_sc
+clear
+echo "Checking VPS"
+clear
+source /var/lib/bzstorevpn/ipvps.conf
+if [[ "$IP2" = "" ]]; then
+domain=$(cat /etc/xray/domain)
+else
+domain=$IP2
+fi
+IP=$(wget -qO- ipinfo.io/ip);
+sstp="$(cat ~/log-install.txt | grep -i SSTP | cut -d: -f2|sed 's/ //g')"
+until [[ $user =~ ^[a-zA-Z0-9_]+$ && ${CLIENT_EXISTS} == '0' ]]; do
+		read -rp "Usernew : " -e user
+		CLIENT_EXISTS=$(grep -w $user /var/lib/bzstorevpn/data-user-sstp | wc -l)
+
+		if [[ ${CLIENT_EXISTS} == '1' ]]; then
+			echo ""
+			echo -e "Username ${RED}${user}${NC} Already On VPS Please Choose Another"
+			exit 1
+		fi
+	done
+read -p "Password : " pass
+read -p "Expired (Days) : " masaaktif
+hariini=`date -d "0 days" +"%Y-%m-%d"`
+exp=`date -d "$masaaktif days" +"%Y-%m-%d"`
+cat >> /home/sstp/sstp_account <<EOF
+$user * $pass *
+EOF
+echo -e "### $user $exp">>"/var/lib/bzstorevpn/data-user-sstp"
+clear
+cat <<EOF
+
+============================
+SSTP VPN
+============================
+IP/Host   : $IP
+Domain    : $domain
+Username  : $user
+Password  : $pass
+Port      : $sstp
+Cert      : http://$IP:89/server.crt
+Created   : $hariini
+Expired   : $exp
+============================
+Script By  @JengkolOnline
+EOF
